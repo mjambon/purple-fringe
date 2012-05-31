7af2ac32b7d1bc04edced54b3b3a5dafb6a11429
Martin Jambon <martin@mjambon.com>
2012-05-31 01:04:22 -0700

    Added license and install target for examples

--- examples/make-examples.sh
+++ examples/make-examples.sh
@@ -2,12 +2,23 @@
 
 print () {
     echo "\
+<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\"
+    \"http://www.w3.org/TR/html4/strict.dtd\">
 <html>
 <head>
-<title>Purple fringing removal examples</title>
+<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\">
+<title>Purple fringe removal examples</title>
 </head>
 <body>
+<h1>Purple fringe removal examples</h1>
+<p>These images were processed with
+<a href=\"https://github.com/mjambon/purple-fringe\"><code>unpurple</code></a>,
+a command-line program that tries to remove
+<a href=\"http://en.wikipedia.org/wiki/Purple_fringing\">purple fringing</a>
+from digital photos.
+</p>
 <table>
+<tr><th>Input</th><th>Output</th></tr>
 "
     for x in $*; do
         echo "\
